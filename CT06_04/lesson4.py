7e338085aa2b919b86a94a7619d258fc3591f2d7
wooof4 <tytus8392@gmail.com>
2025-02-14 09:49:52 +0000

    Feb 14, 2025, 9:49 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,44 @@
-print("Hello from lesson 4")
+# task1
+# name1 = input("whats your name")
+# print(name1)
+# colour = input("whats your favourite colour")
+# print(colour)
+# age = input("whats your age")
+# print (age)
+
+# #task 2
+# print ("hello " + name1)
+# hobby = input("whats your favourite hobby")
+# print ("I enjoy " + (hobby))
+# vacation = input("whats your dream vacation")
+# print ("i would love to visit " + (vacation))
+
+#task 3
+# age2 = input("whats ur your age")
+# age2 = int(age2)
+# age2 = age2 + 50
+# print ("your age is " + str(age2) + " in 50 years" )
+# no = input ("what number do you want")
+# no = int(no)
+# no = no * 2
+# print ("double of your number is " + str(no))
+# yearofbirth = input("whats your birth year")
+# print ("your age is " + str(2025 -int(yearofbirth)))
+#task 4
+message = "hello"
+message = "10"
+message = int(message)
+print (message)
+#task 5
+firstname = "tytus"
+lastname = "no u dont get to know that"
+print (firstname + " " + lastname) 
+#task 6
+username = "woooof"
+userage = 13
+print (username + " is " + str(userage) + " years old")
+
+
+
+
+
