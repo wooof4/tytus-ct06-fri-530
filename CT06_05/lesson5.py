d3c256a2d229457331f65a7a69734638bb87da43
wooof4 <tytus8392@gmail.com>
2025-02-14 10:01:49 +0000

    Feb 14, 2025, 10:01 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,8 +1,8 @@
 # print("Hello from lesson 5")
-var2 = input("whats ur name")
-var3 = input("what age r u turning this year")
-var1 = ("hello")
-print ("happy " + var3 + "th birthday " + var2 + "!" + var1)
+# var2 = input("whats ur name")
+# var3 = input("what age r u turning this year")
+# var1 = ("hello")
+# print ("happy " + var3 + "th birthday " + var2 + "!" + var1)
 for i in range (3):
     print ("woof")
 
