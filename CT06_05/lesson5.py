d7b4c578246b99026ded9249ab2d82c3da1c0522
wooof4 <tytus8392@gmail.com>
2025-02-14 09:59:44 +0000

    Feb 14, 2025, 9:59 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -3,6 +3,6 @@ var2 = input("whats ur name")
 var3 = input("what age r u turning this year")
 var1 = ("hello")
 print ("happy " + var3 + "th birthday " + var2 + "!" + var1)
-for i in range ():
+for i in range (3):
     print ("woof")
 
