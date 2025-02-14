89c5a3ec4cf7ef0d63508e59b84418168e6b4c2e
wooof4 <tytus8392@gmail.com>
2025-02-14 10:05:51 +0000

    Feb 14, 2025, 10:05 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -3,10 +3,11 @@
 # var3 = input("what age r u turning this year")
 # var1 = ("hello")
 # print ("happy " + var3 + "th birthday " + var2 + "!" + var1)
-print ("before the loop")
-for i in range (3):
-    print ("woof")
+# print ("before the loop")
+# for i in range (3):
+#     print ("woof")
 
-print ("outside the loop")
+# print ("outside the loop")
 #task 1
+
 
