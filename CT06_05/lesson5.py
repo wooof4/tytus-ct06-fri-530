bb8cbc9592d414a3ddbfb7dc525adb60b1c07432
wooof4 <tytus8392@gmail.com>
2025-02-14 10:20:30 +0000

    Feb 14, 2025, 10:20 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -19,7 +19,8 @@
 # for i in range (100):
 #     print ("i like cake")
 #     print ("give")
-for i in range (1,60):
-    print (i)
-
+# for i in range (1,60):
+#     print (i)
+for i in  range(1,5):
+    
 
